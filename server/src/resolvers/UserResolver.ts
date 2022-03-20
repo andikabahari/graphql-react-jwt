8650346fac894d5d558d44191d186bfb845f49a7
Andika Bahari <kzlfxx@gmail.com>
2022-03-20 14:50:42 +0700

    Create register mutation and update User entity

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,9 +1,14 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return "Hi!";
+  }
+
+  @Mutation()
+  register(@Arg("email") email: string, @Arg("password") password: string) {
+    return null;
   }
 }
