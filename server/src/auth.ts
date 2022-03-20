dafa063e95f00ffe6ccb13c5dfb8eb972162f7b6
Andika Bahari <kzlfxx@gmail.com>
2022-03-20 16:04:32 +0700

    Add environment variables

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -3,14 +3,14 @@ import { User } from "./entity/User";
 
 export const createAccessToken = (user: User) => {
   const userId = { userId: user.id };
-  const secret = "mylittlesecret1";
-  const expiresIn = { expiresIn: "15m" };
+  const secret = process.env.ACCESS_TOKEN_SECRET!;
+  const expiresIn = { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION_TIME };
   return sign(userId, secret, expiresIn);
 };
 
 export const createRefreshToken = (user: User) => {
   const userId = { userId: user.id };
-  const secret = "mylittlesecret2";
-  const expiresIn = { expiresIn: "7d" };
+  const secret = process.env.REFRESH_TOKEN_SECRET!;
+  const expiresIn = { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION_TIME };
   return sign(userId, secret, expiresIn);
 };
