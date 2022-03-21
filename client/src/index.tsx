2e01e2e25c4b6ffd3690899f75e9b4a940b2b6d5
Andika Bahari <kzlfxx@gmail.com>
2022-03-21 14:19:27 +0700

    Setup GraphQL for client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import ApolloClient from "apollo-boost";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+}) as any;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
