2d2051114cd9ea0e6c1fe7f836beafecc621766a
Andika Bahari <kzlfxx@gmail.com>
2022-03-21 15:24:50 +0700

    Update login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../generated/graphql";
 
 const Login: React.FC = () => {
-  return <div>Login page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login] = useLoginMutation();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h3>Login page</h3>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          const response = await login({
+            variables: {
+              email,
+              password,
+            },
+          });
+
+          if (response.data?.login.accessToken) {
+            navigate("/");
+          }
+        }}
+      >
+        <div>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email"
+            value={email}
+          />
+        </div>
+        <div>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+            value={password}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
