4f9ed480bdd9aefee0ad1bd0f6ebfbffb184b9dc
Andika Bahari <kzlfxx@gmail.com>
2022-03-21 15:12:52 +0700

    Update register page

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -57,12 +57,52 @@ export type User = {
   id: Scalars['Int'];
 };
 
+export type RegisterMutationVariables = Exact<{
+  password: Scalars['String'];
+  email: Scalars['String'];
+}>;
+
+
+export type RegisterMutation = { __typename?: 'Mutation', register: boolean };
+
 export type HelloQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type HelloQuery = { __typename?: 'Query', hello: string };
 
 
+export const RegisterDocument = gql`
+    mutation Register($password: String!, $email: String!) {
+  register(password: $password, email: $email)
+}
+    `;
+export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutation, { data, loading, error }] = useRegisterMutation({
+ *   variables: {
+ *      password: // value for 'password'
+ *      email: // value for 'email'
+ *   },
+ * });
+ */
+export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options);
+      }
+export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
+export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
 export const HelloDocument = gql`
     query Hello {
   hello
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRegisterMutation } from "../generated/graphql";
 
 const Register: React.FC = () => {
-  return <div>Register page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register] = useRegisterMutation();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      Register page
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          const response = await register({
+            variables: {
+              email,
+              password,
+            },
+          });
+
+          if (response.data?.register) {
+            navigate("/");
+          }
+        }}
+      >
+        <div>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email"
+            value={email}
+          />
+        </div>
+        <div>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+            value={password}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
